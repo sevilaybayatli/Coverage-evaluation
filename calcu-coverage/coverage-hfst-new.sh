88411e2d8a4af1f1cd132684483bb8df54ae4972
sevilay bayatlı <sevilaybayatli@gmail.com>
2020-07-16 00:51:15 +0300

    Create coverage-hfst-new.sh

---
+++ calcu-coverage/coverage-hfst-new.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+CORPUS=$1
+ANALYSER=$2
+LG=$(echo $CORPUS | sed 's:.*\/::' | sed -r 's:(.*\..*)\..*\.txt.*:\1:') # get corpus prefix
+#CORP=`echo $CORPUS | sed 's:.*\/::' | sed -r 's/^[\.\/]*([a-z][a-z][a-z])\.([^\.]*?)\..*/\2/'`
+#echo $LG
+#echo $CORP
+#ANALYSERDIR=$APERTIUMPATH/languages/apertium-$LG
+#CORPUS=$APERTIUMPATH/../turkiccorpora/$LG.bible.txt.bz2
+#ANALYSER=$ANALYSERDIR/$LG.automorf.hfst
+
+TMPCORPUS=/tmp/$LG.corpus.txt
+
+if [[ $1 =~ .*bz2 ]]; then
+	CAT="bzcat"
+else
+	CAT="cat"
+fi;
+
+$CAT $CORPUS > $TMPCORPUS
+
+echo "Generating hitparade (might take a bit!)"
+
+cat $TMPCORPUS | apertium-destxt | hfst-proc -w $ANALYSER | apertium-retxt | sed 's/\$\s*/\$\n/g' > /tmp/$LG.parade.txt
+
+echo "TOP UNKNOWN WORDS:"
+
+cat /tmp/$LG.parade.txt | grep '\*' | sort | uniq -c | sort -rn | head -n20
+
+TOTAL=`cat /tmp/$LG.parade.txt | wc -l`
+KNOWN=`cat /tmp/$LG.parade.txt | grep -v '\*' | wc -l`
+UNKNOWN=`cat /tmp/$LG.parade.txt | grep '\*' | wc -l`
+
+PERCENTAGE=`calc $KNOWN/$TOTAL | sed 's/[\s\t]//g'`
+
+echo "coverage: $KNOWN / $TOTAL ($PERCENTAGE)"
+echo "remaining unknown forms: $UNKNOWN"
+
+DATE=`date`;
+echo -e $LG $DATE"\t"$KNOWN"/"$TOTAL"\t"$PERCENTAGE >> history.log
+tail -1 history.log
